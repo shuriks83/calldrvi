1ca1fb8fca92bdbe377d6472b3365ff7fc848f21
shuriks <nagornov@kraitek.ru>
2025-11-17 17:09:08 +0700

    main init

---
+++ calldrvi/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace calldrvi
+{
+    static class Logger
+    {
+        private static readonly object _lock = new object();
+        private static string logFile = "calldrvi.log";
+
+        public static void Write(string text)
+        {
+            lock (_lock)
+            {
+                File.AppendAllText(logFile, text + Environment.NewLine, Encoding.UTF8);
+            }
+        }
+
+        public static void Separator()
+        {
+            Write(new string('-', 60));
+        }
+    }
+}
---
+++ calldrvi/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace calldrvi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Использование: calldrvi.exe \"<command>\""); // тут return _Result(false, "01");  !!!
+                return;
+            }
+
+            string pipeName = "kktpipe";
+            
+            // объединяем все аргументы в одну строку с пробелами
+            string command = string.Join(" ", args);
+
+//            Console.WriteLine(string.Format("Команда: {0}", command));
+
+            try
+            {
+                using (NamedPipeClientStream pipe = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut))
+                {
+                    pipe.Connect(3000); // ждать до 3 сек
+
+                    // явное открытие потоков
+                    StreamWriter writer = new StreamWriter(pipe, Encoding.UTF8);
+                    StreamReader reader = new StreamReader(pipe, Encoding.UTF8);
+
+                    writer.WriteLine(command);
+                    writer.Flush();
+
+                    string response = reader.ReadLine();
+                    Console.WriteLine(response ?? "1 91,нет ответа от демона"); // 
+
+                    writer.Close();
+                    reader.Close();
+                }
+            }
+            catch (TimeoutException)
+            {
+                Console.WriteLine("1 92,не удалось подключиться к демону (тайм-аут)");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("1 93,ошибка обмена " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("1 94,ошибка " + ex.Message);
+            }
+        }
+    }
+
+}
